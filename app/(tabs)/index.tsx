134c506928eea3a0ec432f9d373dd87d3f6bca42
Douglas Dias Leal <douglas_san@hotmail.com>
2024-11-30 23:11:04 +0900

    chore: add Roboto font

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 items-center justify-center bg-gray-600 p-6">
       <Text className='text-white text-3xl'>Groups</Text>
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,34 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import '../global.css';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { ActivityIndicator, View } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [fontsloaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   useEffect(() => {
-    if (loaded) {
+    if (fontsloaded) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [fontsloaded]);
 
-  if (!loaded) {
-    return null;
+  if (!fontsloaded) {
+    return (
+      <View className='items-center justify-center flex-1'>
+        <ActivityIndicator />;
+      </View>
+    )
   }
 
   return (
