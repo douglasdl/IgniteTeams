9fd366b3084f2c18b7649abbd652cea2071a25bc
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-01 10:36:35 +0900

    feat: add GroupCard component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,25 @@
+import { GroupCard } from '@/components/GroupCard';
 import { Header } from '@/components/Header';
 import { Highlight } from '@/components/Highlight';
 import { View } from 'react-native';
 
 export default function HomeScreen() {
+
+  function handleOpenGroup(group: string) {
+		
+	}
+
   return (
     <View className="flex-1 bg-zinc-800 p-6">
       <Header />
       <Highlight 
 				title='Turmas'
 				subtitle='Jogue com a sua turma'
+			/>
+
+      <GroupCard 
+			  title={"Name"}
+			  onPress={() => {handleOpenGroup("")}}
 			/>
     </View>
   );
---
+++ components/GroupCard.tsx
@@ -0,0 +1,19 @@
+import { UsersThree } from "phosphor-react-native";
+import colors from "tailwindcss/colors";
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+interface GroupCardProps extends TouchableOpacityProps {
+  title: string
+}
+
+export function GroupCard({ title, ...rest }: GroupCardProps) {
+  return (
+    <TouchableOpacity
+      className="w-full h-24 bg-zinc-700 rounded-lg flex-row items-center p-6 gap-4"
+      {...rest}
+    >
+      <UsersThree size={32} color={colors.green[700]} weight="fill" />
+      <Text className="text-xl text-gray-200">{title}</Text>
+    </TouchableOpacity>
+  )
+}
