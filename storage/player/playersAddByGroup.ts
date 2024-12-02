04575657ff0cb91d862466462ae83a6a539438dc
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-02 23:52:35 +0900

    feat: add function to create new player in the local storage

---
+++ storage/player/playersAddByGroup.ts
@@ -0,0 +1,23 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { PLAYER_COLLECTION } from '../storageConfig';
+import { AppError } from '@/utils/AppError';
+import { PlayerStorageDTO } from '@/storage/player/PlayerStorageDTO';
+import { playersGetByGroup } from './playersGetByGroup';
+
+export async function playersAddByGroup(newPlayer: PlayerStorageDTO, group: string) {
+  try {
+    const storedPlayers = await playersGetByGroup(group);
+    
+    // Check if the player name already exists
+    const playerAlreadyExists = storedPlayers.filter(player => player.name === newPlayer.name);
+    if(playerAlreadyExists.length > 0) {
+      throw new AppError("Essa pessoa já estáadicionada em um time!");
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPlayer]);
+
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage);
+  } catch (error) {
+    throw error;
+  }
+}
